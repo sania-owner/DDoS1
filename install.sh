e026373b1df7196f0252eb32cf2b822a31e9f3a2
sania-owner <saniaowner@ya.ru>
2022-04-05 14:25:49 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,9 @@
-#!/bin/bash
-
 sudo apt-get update
 mkdir "$HOME"/DDoS
 git clone https://github.com/MHProDev/MHDDoS.git "$HOME"/DDoS/MHProDev
 git clone https://github.com/porthole-ascend-cinnamon/mhddos_proxy.git "$HOME"/DDoS/mhddos_proxy
 sudo apt install python3-pip -y
-cd "$HOME"/DDoS/MHProDev
-pip3 install -r requirements.txt
+cd "$HOME"/DDoS/MHProDev; pip3 install -r requirements.txt
 python3 -m pip install tabulate
 
 wget -O "$HOME"/DDoS/auto.sh https://raw.githubusercontent.com/sania-owner/DDoS1/main/auto.sh; chmod ugo+x "$HOME"/DDoS/auto.sh

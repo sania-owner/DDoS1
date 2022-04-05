5f78898b51547b2a9d98487eab5366b7a9bfbc0d
sania-owner <saniaowner@ya.ru>
2022-04-05 14:02:06 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,6 +11,5 @@ python3 -m pip install tabulate
 
 wget -O "$HOME"/DDoS/mhddos_proxy/auto.sh https://raw.githubusercontent.com/sania-owner/DDoS1/main/auto.sh
 wget -O "$HOME"/DDoS/mhddos_proxy/start.sh https://raw.githubusercontent.com/sania-owner/DDoS1/main/start.sh
-chmod 0770 /root/auto.sh
-chmod 0770 /root/start.sh
-
+chmod 0770 "$HOME"/DDoS/auto.sh
+chmod 0770 "$HOME"/DDoS/start.sh
